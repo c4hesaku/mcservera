8f2f4deea08b6c156265db389e65b1bfbab27440
c4hesaku <azrafchoudhuryy@gmail.com>
2025-04-16 15:53:53 +0600

    Update mcservertermux.sh

--- mcservertermux.sh
+++ mcservertermux.sh
@@ -9,5 +9,6 @@ java -jar paper-1.21.1-133.jar
 echo eula=true > /sdcard/minecraft-server/eula.txt
 touch /sdcard/start.sh 
 echo "cd /sdcard/minecraft-server && java -Xms1536M -Xmx1536M -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar /sdcard/minecraft-server/paper-1.21.1-133.jar" > /sdcard/start.sh
-echo "you can now go to /sdcard and then start the start.sh script using bash."
+chmod +x start.sh
+echo "You can now go to sdcard and then start the start.sh script using bash start.sh."
 read -n 1 -s -r -p "Press any key to continue"
